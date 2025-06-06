4c79cd97bad566d3043159aed542130f5fd15ba1
hemant4106 <412006hemantyadav@gmail.com>
2025-06-06 20:25:23 +0530

    fixed background coloe on mobile devices

--- src/components/Movielistbygenre.jsx
+++ src/components/Movielistbygenre.jsx
@@ -5,7 +5,7 @@ import MovieArray from './MovieArray'
 function Movielistbygenre() {
   const getmovie=()=>{GlobalApi.getMovieGenreById(constgenrelist.genere.id)}
   return (
-    <div>
+    <div className='bg-[#131520]'>
       {constgenrelist.genere.map((item)=>(
        <div className='pt-4 pb-4 w-[100vw]' ><h1 className='text-[22px] font-semibold text-white pl-3 pb-2'>{item.name}</h1> 
        <MovieArray id={item.id} /></div>
